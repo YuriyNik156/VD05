46bef45d422f15a1103e9a4e10a0fb8958b272a2
YuriyNik156 <yuriy.nik156@gmail.com>
2025-06-03 15:39:37 +0300

    Update chess_players.py

--- hometask/chess_players.py
+++ hometask/chess_players.py
@@ -7,15 +7,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def index(password = None):
-    return render_template("index.html")
+    context = {
+        "title" : "Главная страница сайта"
+    }
+    return render_template("home.html", **context)
 
 @app.route("/blog/")
 def blog(password = None):
-    return render_template("blog.html")
-
-@app.route("/contacts/")
-def contacts(password = None):
-    return render_template("contacts.html")
+    context = {
+        "title": "Страница блога"
+    }
+    return render_template("about.html", **context)
 
 if __name__ == "__main__":
     app.run()
